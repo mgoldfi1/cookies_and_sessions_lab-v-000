96d4c18e3e452ade76b0dfa745b8b404851922ae
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-28 16:36:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   def cart 
     cart = [] || session[:cart]
     session[:cart] = cart 
-    session[:cart] ||= []
+    session[:cart]
   end
 end
